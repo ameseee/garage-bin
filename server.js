174082e645280a598db759759d3022109bac1b15
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-18 09:06:42 -0700

    Set up express boilerplate

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') !== 'https') {
+    return response.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  next();
+};
+if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 3000);
+
+app.locals.title = 'Garage Bin';
+
+app.listen(app.get('port'), () => {
+  // eslint-disable-next-line no-console
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+module.exports = app;
